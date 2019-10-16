cc6e14e6e120b646d4a302c2c4019cf11c75c322
Paramita Daniswari <mitadaniswari@gmail.com>
2019-10-16 13:48:12 +0700

    "commit_16102019"

--- application/controllers/api/Cek.php
+++ application/controllers/api/Cek.php
@@ -15,8 +15,10 @@ public function index_get(){
         //ambil data yang punya item code tsb dan 
         //jika ada dibandingkan yang paling terkini
         // samakan uid_nfc, kalo sama => return true, slah return false & uid nya
+    $data_final=array();
         $kode_item=$this->uri->segment('4');
-        $no_lot=$this->uri->segment('5');
+        $uid=$this->uri->segment('5');
+        $no_lot=$this->uri->segment('6');
        // $uid=$this->uri->segment('6');
         $sql_3="SELECT TOP 1 a.no_rcv ,a.uid_nfc uid, a.kode_item_rcv kode_item, a.exp_date exp_date, a.qty_rcv
                 from dbo.nfc_rcv a
@@ -46,7 +48,7 @@ public function index_get(){
                 ORDER BY nih_expired ASC
                 ";
 
-        $sql_2="SELECT TOP 1 a.nic_uid_nfc, a.nic_lot, a.nic_expired, a.nic_qty, a.nic_wh, a.nic_item
+        $sql_2="SELECT TOP 1 a.nic_uid_nfc, a.nic_lot, a.nic_expired, a.nic_qty, a.nic_wh, a.nic_item, a.nic_location
                 from nfc_id_card a
                 where a.nic_expired > getdate()
                 and a.nic_item =$kode_item
@@ -57,7 +59,53 @@ public function index_get(){
 
         $query2 = $this->db->query($sql_2);
         $data2=$query2->result_array();
-        $this->response($data2, REST_Controller::HTTP_OK);
+
+        $sql_3="SELECT 
+        a.nic_lot as lot_card, a.nic_qty as qty_card, a.nic_expired as expired_card, a.nic_location as location_card
+         from nfc_id_card a where a.nic_uid_nfc='$uid'";
+        $query3 = $this->db->query($sql_3);
+        $data3=$query3->result_array();
+
+        $sql_4="SELECT isnull(sum(qty),0) as qty from dbo.nfc_transaksi a
+                 where kode_barang=$kode_item and uid_picked='$uid'
+                 and not EXISTS (Select * from dbo.nfc_wtr_head h where epoch = h.h_wtr_id)";
+        $query4 = $this->db->query($sql_4);
+        $data4=$query4->result_array();
+        foreach ($data4 as $key) {
+            $qty_transaksi=$key["qty"];
+        }
+
+        if( count($data2) > 0 && count($data3)>0 ){
+           // $data_final=["bisa"];
+             foreach ($data3 as $key_card ) {
+               $lot_card=$key_card["lot_card"];
+               $qty_card=$key_card["qty_card"];
+               $expired_card=$key_card["expired_card"];
+               $location_card=$key_card["location_card"];
+            }
+
+            foreach ($data2 as $key ) {
+                //hitung qty gantung
+                $val_ready = number_format($qty_card) - number_format($qty_transaksi);
+              $data_["nic_uid_nfc"]   = $key["nic_uid_nfc"];
+              $data_["nic_lot"]       = $key["nic_lot"];
+              $data_["nic_expired"]   = $key["nic_expired"];
+              $data_["nic_qty"]       = $key["nic_qty"];
+              $data_["nic_wh"]        = $key["nic_wh"];
+              $data_["nic_item"]      = $key["nic_item"];
+              $data_["lot_card"]      = $lot_card;
+              $data_["qty_card"]      = number_format($qty_card);
+              $data_["expired_card"]  = $expired_card;
+              $data_["location_exp"] = $key["nic_location"];
+              $data_["qty_ready"]    = $val_ready;
+              array_push($data_final, $data_);
+            }
+        }else{
+             $data_final=[];
+        }
+
+       
+        $this->response($data_final, REST_Controller::HTTP_OK);
 
     } 
 }
